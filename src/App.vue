dbef8ca055f36b5be60de5fa96182863875b2aaf
Ianz8448 <iapimentelv@gmail.com>
2026-05-13 16:44:02 -0400

    Initial Commit: Basic Layout

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import SearchBar from './components/SearchBar.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Giphy API Search</h1>
+  </header>
+  <main>
+    <SearchBar />
+  </main>
+</template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="search-bar">
+        <label for="search-input">Search for GIFs:</label>
+        <input type="text" id="search-input" placeholder="Search..." />
+        <label for="quantity-input">Limit:</label>
+        <input id="quantity-input" type="number" value="5" min="1" max="24" />
+        <input id="search-button" type="button" value="Search" onclick="run()"></input>
+    </div>
+</template>
