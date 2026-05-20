d1f94283921aa4231049599597fa736f2165d02b
Your Name <you@example.com>
2026-05-20 17:28:26 -0400

    server.js progress

--- src/server/server.js
+++ src/server/server.js
@@ -2,5 +2,43 @@
     Create a simple server (node server) using express
 
     Endpoints:
-    GET /search?q=searchTerm&limit=limit&api_key=api_key
+    GET /search?q=searchTerm&l=limit
 */
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+import config from './config.js';
+const apiKey = config.apiKey;
+
+app.get('/search', async (req, res) => {
+    const {q, l} = req.query;
+    const uri = createURI(q, l);
+    // Make API request using the generated URI
+
+    if (!q) {
+    return res.status(400).json({ error: "Search term 'q' is required" });
+  }
+
+  const response = await fetch(uri);
+
+  if (!response.ok) {
+    return res.status(response.status).json({ error: "Giphy API error" });
+  }
+
+  const data = await response.json();
+  res.json(data);
+});
+
+
+
+function createURI (search, searchQuantity) {
+    return `https://api.giphy.com/v1/gifs/search?q=${search}&limit=${searchQuantity}&api_key=${apiKey}`;
+}
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${3000}/`);
+});
